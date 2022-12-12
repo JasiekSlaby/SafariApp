c6e38d97a30fb378f467e5a175b0727e0570b26c
Jan Słaby <72975366+JasiekSlaby@users.noreply.github.com>
2022-12-12 23:37:20 +0100

    Adding basic project

---
+++ src/test/java/com/SafariApp/SafariApp.java
@@ -0,0 +1,13 @@
+package com.SafariApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafariAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
